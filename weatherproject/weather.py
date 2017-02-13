6324e71196c5b98c2c45c6649b6cc76b6a683f01
DESKTOP-JNR2691\Admin <haroldyewa@gmail.com>
2017-02-13 15:10:14 +0300

    Weather.py iteration update to get specific value

--- weatherproject/weather.py
+++ weatherproject/weather.py
@@ -13,24 +13,30 @@ class Reader:
                 raise ValueError('Filename must end with .dat')
 
 def main():
-    arr = [] #empty array
-    r = Reader() #instantiate Reader Class
+    arr = []  # empty array
+    index = 0
+    r = Reader()  # instantiate Reader Class
     try:
-        dt = r.dataread('weather.dat') #read weather.dat file using the dataread method in Reader Class
-        index = 0
-        for line in dt: #dt iteration to get targeted values from weather.dat file
-            if index != 0 or index != len(dt)-1:
-                dy = line[0] #assigns day to variable dy
-                ln1 = line[1].split('*') #assumption: astericks was eliminated to use the integer value
-                ln2 = line[2].split('*') #assumption: astericks was eliminated to use the integer value
-                mxt = int(ln1[0]) #convert ln[1] string values to integer, assigns MXT value from weather.dat to variable mxt
-                mnt = int(ln2[0]) #convert ln[2] string values to integer, assigns MNT value from weather.dat to variable mnt
-                diff = (mxt-mnt) #get difference of [mxt] and [mnt] variables
-                arr.append(diff) #append difference [diff] values to an array
+        dt = r.dataread('weather.dat')  # read weather.dat file using the dataread method in Reader Class
+        for line in dt:  # dt iteration to get targeted values from weather.dat file
+            dy = line[0]
+            ln1 = line[1].split('*')  # assumption: asterisks was eliminated to use the integer value
+            ln2 = line[2].split('*')  # assumption: asterisks was eliminated to use the integer value
+            mxt = int(ln1[0])  # convert ln[1] string values to integer, assigns MXT value from weather.dat to variable mxt
+            mnt = int(ln2[0])  # convert ln[2] string values to integer, assigns MNT value from weather.dat to variable mnt
+            diff = (mxt - mnt)  # get difference of [mxt] and [mnt] variables
+            if dy not in arr:
+                arr.append([dy, diff])  # append difference [diff] values to an array
 
-            if diff == max(arr):
-                print("{0} {1}".format(dy, diff)) # display of day and maximum spread
-            index+=1
+        for item in arr: #get each array item and iterate
+            mxdiff = max(arr) #get array element with maximum difference
+            set_dy = item[0] #append value to set_dy
+            set_diff = item[1] #append value to set_diff
+            if set_diff == mxdiff[1]: #equate the max difference(mxdiff) with each set_diff value
+                #if the above statement is true the below print will be executed
+                print("{0} {1}".format(set_dy, set_diff))  # display of day and maximum spread
+                break
+
     except IOError as e:
         print("(Exception: ", e)
     except ValueError as e:
